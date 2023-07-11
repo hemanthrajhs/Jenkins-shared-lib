44eac03e41aaf4b81ad87a8fafd90bd31957285f
hemanthrajhs <hemanthrajhs944@gmail.com>
2023-07-11 14:25:16 +0530

    adding file

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,8 +1,15 @@
-def call(String projects, String imageTag, String hubUser) {
-    withCredentials([usernamePassword(credentialsId: 'dockerimage', passwordVariable: 'pass', usernameVariable: 'user')]) {
-         sh "docker login -u '$USER' -p '$PASS'"
-    }
+// def call(String projects, String imageTag, String hubUser) {
+//     withCredentials([usernamePassword(credentialsId: 'dockerimage', passwordVariable: 'pass', usernameVariable: 'user')]) {
+//          sh "docker login -u '$USER' -p '$PASS'"
+//     }
     
-    sh "docker image push ${hubUser}/${projects}:${imageTag}"
-    sh "docker image push ${hubUser}/${projects}:latest"
+//     sh "docker image push ${hubUser}/${projects}:${imageTag}"
+//     sh "docker image push ${hubUser}/${projects}:latest"
+// }
+def call(String appname, String region, String aws-account-id){
+
+    sh"""
+    aws ecr get-login-password --region {$region} | docker login --username AWS --password-stdin ${aws-account-id}.dkr.ecr.${region}.amazonaws.com
+    docker push ${aws-account-id}.dkr.ecr.${region}.amazonaws.com/${appname}:latest
+    """
 }
--- vars/dockerscan.groovy
+++ vars/dockerscan.groovy
@@ -1,6 +1,12 @@
-def call(String projects, String imageTag, String hubUser) {
-    sh """
-    trivy image ${hubUser}/${projects}:${imageTag} > scan.txt
-    cat scan.txt
-    """
+// def call(String projects, String imageTag, String hubUser) {
+//     sh """
+//     trivy image ${hubUser}/${projects}:${imageTag} > scan.txt
+//     cat scan.txt
+//     """
+// }
+def call(String appname, String region, String aws-account-id) {
+     sh """
+     trivy image ${aws-account-id}.dkr.ecr.${region}.amazonaws.com/${appname}:latest
+     cat scan.txt
+     """
 }
